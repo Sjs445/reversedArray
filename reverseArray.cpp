526ae0e92741b7cea23032748c01a21710dae4de
Sjs445 <47377055+Sjs445@users.noreply.github.com>
2019-02-05 21:51:55 -0800

    Add files via upload

---
+++ reverseArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+void reverseArray(int arr[], int const size);
+
+int main()
+{
+  int const size=5;
+  int arr[size]={1, 2, 3, 4, 5};
+
+cout<<"Array original..\n";
+  for(int i=0; i<size; i++)
+  {
+    cout<<arr[i]<<"\n";
+  }
+
+reverseArray(arr, size);
+
+  return 0;
+}
+
+void reverseArray(int arr[], int const size)
+{
+  for(int i=size; i>0; i--)
+  {
+    cout<<arr[i]<<"\n";
+  }
+}
